8ec7b9b4e39f28276eaf2ba099fbeade306cc924
NicklasLMJensen <Nicklaslmjensen@gmail.com>
2025-08-11 08:58:26 +0200

    Delete hardware/firmware/weather_publisher/Arduino_secrets.h

--- hardware/firmware/weather_publisher/Arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-// arduino_secrets.h
-#define SECRET_SSID   "h4prog"
-#define SECRET_PASS   "1234567890"
-#define MQTT_BROKER   "test.mosquitto.org"  // or your server’s IP/DNS
-#define MQTT_PORT     1883
